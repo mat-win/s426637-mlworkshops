686f1764cff00143e6b83ffecd65d033384e9265
Mateusz Winnicki <mateusz_winnicki@o2.pl>
2020-04-03 19:50:22 +0200

    score

--- scrypt.sh
+++ scrypt.sh
@@ -8,7 +8,7 @@ awk 'BEGIN{FS=OFS="\t"}{print $0,"(sp1_"NR")"}' < reference.txt > reference.trn
 
 awk 'BEGIN{FS=OFS="\t"}{print $0,"(sp1_"NR")"}' < hypothesis.txt > hypothesis.trn
 
-sclite -f 0 -r ref.trn trn -h hyp.trn trn -e utf-8 -i rm -o pra stdout | grep "Scores:" | sed 's/Scores: (#C #S #D #I) // ' | awk '{ print ($2+$3+$4)/($1+$2+$3)}' | tail -n10
+sclite -f 0 -r reference.trn trn -h hypothesis.trn trn -e utf-8 -i rm -o pra stdout | grep "Scores:" | sed 's/Scores: (#C #S #D #I) // ' | awk '{ print ($2+$3+$4)/($1+$2+$3)}' | tail -n10
 
 sclite -f 0 -r reference.trn trn -h hypothesis.trn trn -e utf-8 -i rm -o all stdout | grep 'Scores' > score
 
