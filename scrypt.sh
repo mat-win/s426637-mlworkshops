44cd151c07040cd51e9b8367ac8eb9b934ba7349
Mateusz Winnicki <mateusz_winnicki@o2.pl>
2020-04-03 20:00:29 +0200

    score

--- scrypt.sh
+++ scrypt.sh
@@ -8,15 +8,13 @@ awk 'BEGIN{FS=OFS="\t"}{print $0,"(sp1_"NR")"}' < reference.txt > reference.trn
 
 awk 'BEGIN{FS=OFS="\t"}{print $0,"(sp1_"NR")"}' < hypothesis.txt > hypothesis.trn
 
-sclite -f 0 -r reference.trn trn -h hypothesis.trn trn -e utf-8 -i rm -o pra stdout | grep "Scores:" | sed 's/Scores: (#C #S #D #I) // ' | awk '{ print ($2+$3+$4)/($1+$2+$3)}' | tail -n10
-
 sclite -f 0 -r reference.trn trn -h hypothesis.trn trn -e utf-8 -i rm -o all stdout | grep 'Scores' > score
 
 cat score | tail -n 10
 
-cat score | awk 'BEGIN{OFS="\t"}{print $6, $7, $8, $9}' | awk '{print ($2, $3, $4)/($2, $3, $1}' | tail -n 10
+cat score | awk 'BEGIN{OFS="\t"}{print $6, $7, $8, $9}' | awk '{print ($2, $3, $4)/($2, $3, $1)}' | tail -n 10
 
-cat score | awk 'BEGIN{OFS="\t"}{print $6, $7, $8, $9}' | awk '{print ($2, $3, $4)/($2, $3, $1}' > wer.tsv
+cat score | awk 'BEGIN{OFS="\t"}{print $6, $7, $8, $9}' | awk '{print ($2, $3, $4)/($2, $3, $1)}' > wer.tsv
 
 
 
