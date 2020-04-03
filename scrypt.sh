bd6c512f128793f93bb22be1414a90dfd8970d9c
Mateusz Winnicki <mateusz_winnicki@o2.pl>
2020-04-03 18:26:26 +0200

    TRN sclite

--- scrypt.sh
+++ scrypt.sh
@@ -1,3 +1,18 @@
 wc -l wikiniews_results.tsv > lwierszy.txt
+
+cut -f2 wikiniews_results.tsv > hypothesis.txt
+cut -f3 wikiniews_results.tsv > reference.txt
+
 awk 'BEGIN{FS=OFS="\t"}{print $0,"(sp1_"NR")"}' wikiniews_results.tsv > wikiniews_results.trn
+
+awk 'BEGIN{FS=OFS="\t"}{print $0,"(sp1_"NR")"}' < wikiniews_results.tsv > wikiniews_results.trn
+
+awk 'BEGIN{FS=OFS="\t"}{print $0,"(sp1_"NR")"}' < reference.txt > reference trn
+
+awk 'BEGIN{FS=OFS="\t"}{print $0,"(sp1_"NR")"}' < hypothesis.txt > hypothesis.trn
+
+sclite -f 0 -r reference.trn trn -h hypothesis.trn trn -e utf-8 -i rm -o all stdout | grep Scores > scores
+
 ls -l
+
+
