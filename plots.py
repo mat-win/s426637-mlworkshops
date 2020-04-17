8594cedc74901b4385305f85672e0a5850a203cb
Mateusz Winnicki <mateusz_winnicki@o2.pl>
2020-04-17 19:25:41 +0200

    create plots

--- plots.py
+++ plots.py
@@ -2,31 +2,34 @@
 import matplotlib.pyplot as plt
 import sys
 
-def read_f(filename):
+
+def read_file(filename):
     values = []
-    with open(filename) as f_name:
-        for line in f_name:
+    with open(filename) as f:
+        for line in f:
             values.append(float(line.rstrip('\n')))
     return values
 
+
 def main():
-    in_f = sys.argv[1]
-    out_f = sys.argv[2]
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
 
-    values = read_f(in_f)
+    values = read_file(input_file)
+
     plt.plot(values)
-	
-    plt.xlabel('run')
 
-    y_name = in_f.split('.')[0]
-    plt.ylabel(y_name)
-	
-    plt.savefig(out_f, dpi=800)
+    plt.xlabel('executes')
+
+    ylabel_name = input_file.split('.')[0]
+    plt.ylabel(ylabel_name)
+
+    plt.savefig(output_file, dpi=800)
     plt.close()
+
 
 if __name__ == "__main__":
     main()
-	
 
 
 
